9e497455762a55c5d434895d85a58fddedeae0ef
jatin dhamija <jatindhamij025@gmail.com>
2023-11-25 20:04:48 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const dotenv = require("dotenv")
-
+const colors = require('colors')
 const { chats } = require('./data/data')
+const connectDB = require('./config/db')
 dotenv.config()
+
+connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -19,4 +22,4 @@ app.get("/api/chat/:id", (req, res) => {
     res.send(singleChat)
 })
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server Start on PORT ${PORT} `))
+app.listen(PORT, console.log(`Server Start on PORT ${PORT} `.yellow.bold))
